c86dbf61a3cfb141304af0567e9c03fc3c891f6e
Michael Farber Brodsky <misha@farbebrodsky.com>
2025-12-20 18:31:24 +0200

    small diff

--- home/syncthing.nix
+++ home/syncthing.nix
@@ -1,12 +1,12 @@
 { config, pkgs, inputs, lib, ... }:
 
-let myOptions = import inputs.my-sync.syncthing; in
+let myOptions = (import inputs.my-sync).syncthing; in
 {
   services.syncthing = lib.mkIf config.misha.syncthing.enable {
     enable = true;
     settings.options = {
       relaysEnabled = true;
-      urAccepted = 1;  # accept usage reporting
+      urAccepted = 3;  # accept usage reporting
     };
     settings.devices = myOptions.devices;
     settings.folders = myOptions.folders;
