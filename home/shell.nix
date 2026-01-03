8b9ddfeb4de67d28d6ec3e08585f9d2efda24a17
Michael Farber Brodsky <misha@farbebrodsky.com>
2026-01-03 16:38:55 +0200

    ssh agent

--- home/shell.nix
+++ home/shell.nix
@@ -10,4 +10,5 @@
   programs.bash.shellAliases."cfg" = "cd /persist/nix/";
   programs.bash.initExtra = builtins.readFile ./dotfiles/bashprompt.sh;
   home.file.".gitstatus.sh".source = ./dotfiles/gitstatus.sh;
+  services.ssh-agent.enable = true;
 }
