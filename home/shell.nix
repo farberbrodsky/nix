acd5aa2db72c059ad43f8eddc5ebcf1a29c16966
Michael Farber Brodsky <misha@farbebrodsky.com>
2026-04-18 16:40:05 +0300

    updates

--- home/shell.nix
+++ home/shell.nix
@@ -16,7 +16,8 @@
   programs.bash.shellAliases."cfg" = "cd /persist/nix/";
   programs.bash.initExtra = ''
     eval "$(zoxide init bash)"
-  '' + builtins.readFile ./dotfiles/bashprompt.sh;
+  ''
+  + builtins.readFile ./dotfiles/bashprompt.sh;
   home.file.".gitstatus.sh".source = ./dotfiles/gitstatus.sh;
   services.ssh-agent.enable = true;
   programs.direnv.enable = true;
